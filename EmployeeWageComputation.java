0a8a39cf382cf4c1b41073073fab45e0bb13eac1
nayana005 <nayanasuresh1005@gmail.com>
2022-08-18 22:33:37 +0530

    Added: Daily Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,20 +3,33 @@ package com.bridgelabz.emp_wage10;
 public class EmployeeWageComputation {
 
 	static final int isFullTime = 1;
+	static final int empRatePerHour = 20;
 
 	public static void main(String[] args) {
-		empCheck();
+		dailyEmployeeWage();
 	}
 
-	// Method to check employee is present or absent by using random function
-	public static void empCheck() {
+	// Method to Calculate daily employee wage
+	public static void dailyEmployeeWage() {
 
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == isFullTime)
+		// Variables
+		int empHour = 0;
+		int empWage = 0;
+		double empCheck = Math.floor(Math.random() * 10 ) % 2;
+
+		if(empCheck == isFullTime) {
 			System.out.println("Employee is present !");
-		else
+			empHour = 8;
+		}
+		else {
 			System.out.println("Employee is absent !");
+			empHour = 0;
+		}
+		empWage = empHour * empRatePerHour;
+		System.out.println("Daily Employee Wage = " +empWage);
 	}
 }
+
+
 
 
